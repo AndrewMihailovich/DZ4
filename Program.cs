da85dac732e9f9264094fc83aefb2684b0fb92b8
Andrew <Andy>
2023-08-04 22:58:33 +0300

    DZ4

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число N  = ");
+int N = Convert.ToInt32(Console.ReadLine());
+int Q = 2;
+for (int i = 1; i < N; i++)
+{
+int result = Q % 2;
+if (result == 0)
+{
+Console.Write(Q);
+Q++;
+}
+else
+{
+Q++;
+}}
